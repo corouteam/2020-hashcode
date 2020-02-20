41d533223d9f6c70dd0c672e7fce3c7644fd387f
Anna Labellarte <anna.labellarte.al@gmail.com>
2020-02-20 19:47:31 +0100

    ora

--- src/NotHappyParser.kt
+++ src/NotHappyParser.kt
@@ -2,7 +2,7 @@ import java.io.File
 
 class NotHappyParser{
     fun parse(){
-        File("/a_example.txt").forEachLine {
+        File("a_example.txt").forEachLine {
             println(it)
         }
     }
